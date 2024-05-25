3b7b05cdc1f936e09ca6cfb167d54fcc8a6d20f0
Kummari Narendra <92687869+NarendraShalivahana@users.noreply.github.com>
2024-05-25 13:42:04 +0530

    Update Encapsulation.java

--- OOP/Encapsulation.java
+++ OOP/Encapsulation.java
@@ -1,1 +1,27 @@
-
+//Encapsulation
+class plant{
+    private int age;
+    private int leaves;
+    
+    plant(int age,int leaves){
+        this.age=age;
+        this.leaves=leaves;
+    }
+    
+    public void get_age(){
+        System.out.print(this.age);
+    }
+    
+    public void get_leaves(){
+        System.out.print(this.leaves);
+    }
+}
+// This defined for data binding for data memebers and member functions, to form into a single unit (object of a class), also provide certain layer of abstraction through restricting direct acess to attributes, and can be accessed by (setters and getters) so we can get control over data modification, security over data.
+public class Main {
+    public static void main(String[] args) {
+        plant p=new plant(10,4);
+        p.get_age();
+        p.get_leaves();
+        System.out.print(p.age);//24: error: age has private access in plant [in Main.java]System.out.print(p.age);
+    }
+}
