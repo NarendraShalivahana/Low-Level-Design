e5a11444023ca04bf998b4e01bd92a7710f95de0
Kummari Narendra <92687869+NarendraShalivahana@users.noreply.github.com>
2024-06-17 04:06:39 +0530

    Create HDFC.java

---
+++ Design Patterns/Adapter/HDFC.java
@@ -0,0 +1,7 @@
+// These are the two classes called by respective bank Adapters to get data and work done
+class HDFC{
+    public Integer balance();
+    public String pay(Integer ID);
+    public List<String> getHistory();
+    
+}
